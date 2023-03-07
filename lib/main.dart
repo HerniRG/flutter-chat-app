138f5f4514c91a62db89a50842ba8fc4aab01978
Hernan Rodriguez <hernanrg85@gmail.com>
2023-03-07 16:49:23 +0100

    seccion 7

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,14 @@ import 'package:flutter/material.dart';
 import 'package:chat/routes/routes.dart';
 
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Chat App',
-      initialRoute: 'login',
+      initialRoute: 'chat',
       routes: appRoutes,
     );
   }
